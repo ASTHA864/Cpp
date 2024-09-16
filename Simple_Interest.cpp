27cccdd5f6e149bf3b4afcd66ef3affe22121c7d
Astha Gupta <148465339+ASTHA864@users.noreply.github.com>
2024-09-16 22:01:53 +0530

    Add files via upload

---
+++ Simple_Interest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int P, R, T, SI;
+    cout << "Enter value of P:";
+    cin >> P;
+    cout << "Enter value of P:";
+    cin >> R;
+    cout << "Enter value of P:";
+    cin >> T;
+    SI = (P * R * T) / 100;
+    cout << "Simple Interest:" << SI;
+    return 0;
+}
