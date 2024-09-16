bea46d846bcc4aa6154ed67df496efd0c5b1c7c5
Astha Gupta <148465339+ASTHA864@users.noreply.github.com>
2024-09-16 22:51:42 +0530

    Add files via upload

---
+++ Positive_or_Negative.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter value:";
+    cin >> n;
+    if (n > 0)
+    {
+        cout << "Positive";
+    }
+    else
+    {
+        cout << "Negative";
+    }
+}
